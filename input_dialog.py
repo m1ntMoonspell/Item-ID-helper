ec89d305e1cb4fac8f4f7c9855a8b459c952e75b
m1ntMoonspell <zy1752706779@gmail.com>
2025-12-31 17:00:42 +0800

    unfished choice about pets and teleport

--- input_dialog.py
+++ input_dialog.py
@@ -1,18 +1,25 @@
-from PySide6.QtWidgets import (QPushButton,QApplication,QToolTip,
+from PySide6.QtWidgets import (QPushButton,QToolTip,
                                QDialog,QVBoxLayout,QLineEdit,)
 from PySide6.QtGui import QGuiApplication
 
 class InputDia(QDialog):
-    def __init__(self,text,parent=None):
+    def __init__(self,text,title,parent=None):
         super().__init__(parent)
+        self.creat_subwidgets(text,title)
+
+    def creat_subwidgets(self,text,title):
         self.edit = QLineEdit(self,placeholderText="Enter text here")
         self.button = QPushButton("Confirm")
+        if title == "重置宠物CD" or title == "觉醒单个宠物":
+            self.button.setToolTip()
+
         self.button.clicked.connect(lambda checked,
                                     t=text:self.toclip(t))
         layout = QVBoxLayout(self)
         layout.addWidget(self.edit)
         layout.addWidget(self.button)
         self.button.clicked.connect(self.reject)
+
 
     def toclip(self,text):
         cb = QGuiApplication.clipboard()
--- quick_gm.py
+++ quick_gm.py
@@ -9,13 +9,10 @@ class GMList(QDialog):
         self.setWindowTitle("Quick GM")
         layout = QVBoxLayout(self)
         for k,v in list_dict.items():
-            if k == "重置宠物CD":
-                button = QPushButton(f"{k}")
-                
-            if k == "Teleport":
+            if k == "重置宠物CD" or k == "觉醒单个宠物" or k == "Teleport":
                 button = QPushButton(f"{k}")
                 button.clicked.connect(lambda checked,
-                                       t=v:self.show_input(t))
+                                       t=v:self.show_input(t,k))
             else:
                 button = QPushButton(f"{k}")
                 button.clicked.connect(lambda checked,
@@ -27,8 +24,8 @@ class GMList(QDialog):
         cb = QGuiApplication.clipboard()
         cb.setText(text)
 
-    def show_input(self,text):
-        inDia = InputDia(text)
+    def show_input(self,text,keys):
+        inDia = InputDia(text,keys)
         if inDia.exec():
             inDia.show()
             return inDia
